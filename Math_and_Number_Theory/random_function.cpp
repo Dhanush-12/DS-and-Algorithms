60d21ffe094abed7894ed78520ac92f244d3acf0
Dhanush-12 <dhanushdasarla@gmail.com>
2024-06-19 22:53:42 +0530

    Random function code added

---
+++ Math_and_Number_Theory/random_function.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main()
+{
+    for(int i=1;i<25;i++)
+    {
+        int d=rand()%7;
+        if(d==0)
+        {
+            cout<<1<<endl;
+        }
+        else
+            cout<<d<<endl;
+    }
+    //for dice rolling put mod 6
+}

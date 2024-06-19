4d545e5ec1ef6a178b8c8bd742d83d2812bd5e8c
Dhanush-12 <dhanushdasarla@gmail.com>
2024-06-19 19:44:36 +0530

    prime number code added

---
+++ Math_and_Number_Theory/prime.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool isprime(int n)
+{
+    if(n<2) return false;
+    for(int i=2;i*i<=n;i++)
+    {
+        if(n%i==0) return 0;
+    }
+    return 1;
+}
+int main()
+{
+    int n;
+    cin>>n;
+    if(isprime(n))
+    {
+        cout<<"IS PRIME"<<endl;
+    }
+    else
+    {
+        cout<<"NOT PRIME"<<endl;
+    }
+    return 0;
+}

30e2c2aec29177f63f044e6c7e4089a6812b9492
Dhanush-12 <dhanushdasarla@gmail.com>
2024-07-02 18:59:33 +0530

    Wave sort code added

---
+++ Sorting_Algos/Wave_sort.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+void wave(int *arr,int n)
+{
+    for(int i=1;i<n;i+=2)
+    {
+        if(arr[i]>arr[i-1])
+        {
+            swap(arr[i],arr[i-1]);
+        }
+        if(arr[i]>arr[i+1] && i<=n-2)
+        {
+            swap(arr[i],arr[i+1]);
+        }
+    }
+}
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    cout<<"Array after sorting: "<<endl;
+    wave(arr,n);
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+
+}

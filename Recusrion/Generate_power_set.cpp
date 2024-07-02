9917ba48089f8ac161c1414b3a796a65922139b4
Dhanush-12 <dhanushdasarla@gmail.com>
2024-07-02 10:28:00 +0530

    Generate powerset of array added

---
+++ Recusrion/Generate_power_set.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> pwset(vector<int> v)
+{
+    vector<vector<int>> result;
+    vector<int> ans;
+    int n = v.size();
+    for (int i = 0; i < (1 << n); i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if ((i & (1 << j)) != 0)
+            {
+                ans.push_back(v[j]);
+            }
+        }
+        result.push_back(ans);
+        ans.clear();
+    }
+    return result;
+}
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    vector<vector<int>> ans = pwset(arr);
+    for (int i = 0; i < ans.size(); i++)
+    {
+        for (int j = 0; j < ans[i].size(); j++)
+        {
+            cout << ans[i][j] << " ";
+        }
+        cout << endl;
+    }
+}

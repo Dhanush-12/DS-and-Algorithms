18211f201f8b38a2d4358bae4f263e8904873b27
Dhanush-12 <dhanushdasarla@gmail.com>
2024-08-05 15:31:17 +0530

    for testing large test cases

--- Binary_Search/search_element.cpp
+++ Binary_Search/search_element.cpp
@@ -7,6 +7,7 @@ int bin_search(int n,vector<int>&arr,int k)
     while(low<=high)
     {
         int mid=(low+high)>>1;
+        // for large tests, - mid=
         if(arr[mid]==k) return mid+1;
         else if(arr[mid]>k) high=mid-1;
         else low=mid+1;

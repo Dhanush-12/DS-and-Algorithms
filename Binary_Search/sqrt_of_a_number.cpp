f68ccacf9826564ed045895c03f54bd4143b5a68
Dhanush-12 <dhanushdasarla@gmail.com>
2024-08-08 15:14:21 +0530

    floor of sqrt od a number is added

---
+++ Binary_Search/sqrt_of_a_number.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+#define nl endl
+using namespace std;
+int floor_of_sqrt(int n)
+{
+    int low=1,high=n;
+    int mid,ans=0;
+    while(low<=high)
+    {
+        mid=(low+(high-low)/2);
+        if(mid*mid<=n)
+        {
+            ans=mid;
+            low=mid+1;
+        }
+        else high=mid-1;
+    }
+    return ans;
+}
+int main()
+{
+    while(1){
+    int n;
+    cin>>n;
+    cout<<floor_of_sqrt(n)<<endl;
+    }
+}

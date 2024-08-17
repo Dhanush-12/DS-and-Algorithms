8e3f21b6460b577a4855d2e77cbfe2801235700a
Dhanush-12 <dhanushdasarla@gmail.com>
2024-08-17 14:28:09 +0530

    Minimum number of bits to flip to convert a number to another number code added

---
+++ Bit_Manipulation/Min_bits_to_change_to_convert_num.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int min_bits_to_goal(int n,int m)
+{
+    int ans=n^m;
+    //return __builtin_popcount(ans);// Time complexity : O(log(max(n,m)));
+    int cnt=0;
+    while(ans)
+    {
+        cnt++;
+        ans=(ans&(ans-1));
+    }
+    return cnt; // Time complexiity : O(Number of set bits
+}
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+    cout<<min_bits_to_goal(n,m)<<endl;
+}
+// 13 - 1101
+// 0 - based indexing from the right sides

b27e1b6414892f104f6c2f0b6c97ad72694d2136
Dhanush-12 <dhanushdasarla@gmail.com>
2024-08-17 13:02:36 +0530

    Set and clear the i-th bit code added

---
+++ Bit_Manipulation/set_and_clear_ith_bit.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+// Time complexity : O(1);
+void setithbit(int &n,int k)
+{
+    n = n|(1<<k);
+}
+void clearithbit(int &n,int k)
+{
+    n = n & (~(1<<k));
+}
+int main()
+{
+    int n,k;
+    cin>>n>>k;
+    setithbit(n,k);
+    cout<<n<<endl;
+    clearithbit(n,k);
+    cout<<n<<endl;
+}
+// 13 - 1101
+// 0 - based indexing from the right sides

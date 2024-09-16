3c8f8990c3b1f3d70c395936c8674d6b2cc1d402
Dhanush-12 <dhanushdasarla@gmail.com>
2024-09-16 19:03:45 +0530

    Infix to prefix conversion code added

---
+++ Stacks_ans_Queues/infix_to_prefix.cpp
@@ -0,0 +1,77 @@
+#include<bits/stdc++.h>
+using namespace std;
+int priority(char ch)
+{
+    if(ch=='^') return 3;
+    if(ch=='*' || ch=='/') return 2;
+    if(ch=='+' || ch=='-') return 1;
+    return -1;
+}
+string infix_to_prefix(string str)
+{
+    int n=(int)str.size();
+    string ans;
+    stack<char>s;
+    reverse(str.begin(),str.end());
+    for(int i=0;i<n;i++)
+    {
+        if(str[i]=='(') str[i]=')';
+        else if(str[i]==')') str[i]='(';
+    }
+    int i=0;
+    while(i<n)
+    {
+        if((str[i]>='A' && str[i]<='Z') || (str[i]>='a' && str[i]<='z') || (str[i]>='0' && str[i]<='9'))
+        {
+            ans+=str[i];
+        }
+        else if(str[i]=='(')
+        {
+            s.push(str[i]);
+        }
+        else if(str[i]==')')
+        {
+            while(!s.empty() && s.top()!='(')
+            {
+                ans+=s.top();
+                s.pop();
+            }
+            s.pop();
+        }
+        else
+        {
+            if(str[i]=='^')
+            {
+                while(!s.empty() && priority(str[i])<=priority(s.top()))
+                {
+                    ans+=s.top();
+                    s.pop();
+                }
+            }
+            else
+            {
+                while(!s.empty() && priority(str[i])<priority(s.top()))
+                {
+                    ans+=s.top();
+                    s.pop();
+                }
+            }
+            s.push(str[i]);
+        }
+        i++;
+    }
+    while(!s.empty())
+    {
+        ans+=s.top();
+        s.pop();
+    }
+    reverse(ans.begin(),ans.end());
+    return ans;
+}
+int main()
+{
+    string str;
+    cin>>str;
+    string ans=infix_to_prefix(str);
+    cout<<ans<<endl;
+}

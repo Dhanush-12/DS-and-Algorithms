09727c5a512b67ef35cf08c6d4163e1504bcaf2a
Dhanush-12 <dhanushdasarla@gmail.com>
2025-01-18 15:41:48 +0530

    Space Optimization code added with time and space complexities

--- Dynamic_Programming/Fibinocci.cpp
+++ Dynamic_Programming/Fibinocci.cpp
@@ -1,18 +1,41 @@
 #include<bits/stdc++.h>
 using namespace std;
-// Time complexity of recursion is O(2^n) and space complexity is O(n)
+// Time Complexity: O(2^n) and Space Complexity: O(n)
 int recursion(int n)
 {
     if(n<=1) return n;
     return recursion(n-1)+recursion(n-2);
 }
-// Time complexity of memorization is O(n) and space complexity is O(n)
+// Time Complexity: O(n) and Space Complexity: O(n)
 int memorization(int n,vector<int>&dp)
 {
     if(n<=1) return n;
     if(dp[n]!=-1) return dp[n];
     dp[n]=memorization(n-1,dp)+memorization(n-2,dp);
     return dp[n];
+}
+// Time Complexity: O(n) and Space Complexity: O(n)
+int tabulation(int n,vector<int>&dp)
+{
+    dp[0]=0,dp[1]=1;
+    for(int i=2;i<=n;i++)
+    {
+        dp[i]=dp[i-1]+dp[i-2];
+    }
+    return dp[n];
+}
+// Time Complexity: O(n) and Space Complexity: O(1)
+int spaceOptimization(int n)
+{
+    if(n<=1) return 1;
+    int prev=0,curr=1;
+    for(int i=2;i<=n;i++)
+    {
+        int temp=prev+curr;
+        prev=curr;
+        curr=temp;
+    }
+    return curr;
 }
 int main()
 {
@@ -22,11 +45,6 @@ int main()
     vector<int>dp(n+1,-1);
     cout<<"Answer using memorization: "<<memorization(n,dp)<<endl;
     vector<int>dp1(n+1,0);
-    dp[0]=0,dp[1]=1;
-    for(int i=2;i<=n;i++)
-    {
-        dp[i]=dp[i-1]+dp[i-2];
-    }
-    // Time complexity of tabulation is O(n) and space complexity is O(n)
-    cout<<"Answer using tabulation: "<<dp[n]<<endl;
+    cout<<"Answer using tabulation: "<<tabulation(n,dp1)<<endl;
+    cout<<"Answer using Space Optimization: "<<spaceOptimization(n)<<endl;
 }

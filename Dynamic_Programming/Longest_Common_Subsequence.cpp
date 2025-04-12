e34a22893b53cd821f75e385dfecc9028ea5d700
Dhanush-12 <dhanushdasarla@gmail.com>
2025-04-12 12:39:03 +0530

    Updated the fourth follow up question and clue

--- Dynamic_Programming/Longest_Common_Subsequence.cpp
+++ Dynamic_Programming/Longest_Common_Subsequence.cpp
@@ -4,6 +4,7 @@ using namespace std;
 // Problem Statement: Given two strings s and t, return the length of their longest common subsequence.
 // For the Longest common palindroic subsequence, reverse the string t and find the LCS of s and t.
 // For Minimum Insertions to Make String Palindrome, find the LCPS of s and reverse(s) and return n-LCS.
+// For Mminimum Insetions and Deletions to make the string s as t, find the LCS of s and t and return (n+m-2*LCS).
 // Time Complexity: O(2^n) and Space Complexity: O(n+m);
 int recursion(int ind1,int ind2,string s,string t)
 {

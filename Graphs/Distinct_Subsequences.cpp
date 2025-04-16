05de433ea434ffca55ab8da4044da49de1a2aa76
Dhanush-12 <dhanushdasarla@gmail.com>
2025-04-16 20:06:02 +0530

    Space optmization method added

--- Graphs/Distinct_Subsequences.cpp
+++ Graphs/Distinct_Subsequences.cpp
@@ -1,7 +1,6 @@
 #include<bits/stdc++.h>
 #define inf 1000000007
 using namespace std;
-// Time Complexity: O(2^n) and Space Complexity: O(n);
 int recursion(int ind1,int ind2,string s,string t)
 {
     if(ind2<0) return 1;
@@ -14,7 +13,6 @@ int recursion(int ind1,int ind2,string s,string t)
 
     return recursion(ind1-1,ind2,s,t);
 }
-// Time Complexity: O(n*m) and Space Complexity: O(n*m);
 int memorization(int ind1,int ind2,string s,string t,vector<vector<int>>&dp)
 {
     if(ind2<0) return 1;
@@ -29,7 +27,6 @@ int memorization(int ind1,int ind2,string s,string t,vector<vector<int>>&dp)
 
     return memorization(ind1-1,ind2,s,t,dp);
 }
-// Time Complexity: O(n*m) and Space Complexity: O(n*m);
 int tabulation(int n,int m,string s,string t,vector<vector<int>>&dp)
 {
     for(int i=0;i<n;i++) dp[i][0]=1;
@@ -50,6 +47,28 @@ int tabulation(int n,int m,string s,string t,vector<vector<int>>&dp)
     }
     return dp[n][m];
 }
+// Time complexity: O(n*m) and Space complexity: O(m)
+int space_optimization(int n,int m,string s,string t)
+{
+    vector<int>prev(m+1,0),curr(m+1,0);
+    prev[0]=curr[0]=1;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=m;j++)
+        {
+            if(s[i-1]==t[j-1])
+            {
+                curr[j]=prev[j-1]+prev[j];
+            }
+            else
+            {
+                curr[j]=prev[j];
+            }
+        }
+        prev=curr;
+    }
+    return prev[m];
+}
 int main()
 {
     int n,m;
@@ -61,4 +80,5 @@ int main()
     cout<<"Answer using Memorization: "<<memorization(n-1,m-1,s,t,dp)<<endl;
     vector<vector<int>>dp1(n+1,vector<int>(m+1,0));
     cout<<"Answer using Tabulation: "<<tabulation(n,m,s,t,dp1)<<endl;
+    cout<<"Answer using Space optimization: "<<space_optimization(n,m,s,t)<<endl;
 }

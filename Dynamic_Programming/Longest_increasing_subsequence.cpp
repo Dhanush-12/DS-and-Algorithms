e56bb1a494bfd5d00aff997afdfad9c256962a2c
Dhanush-12 <dhanushdasarla@gmail.com>
2025-05-05 19:22:07 +0530

    Longest increasing subsequence problem with recursion and memorization code added

---
+++ Dynamic_Programming/Longest_increasing_subsequence.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+#define inf 1000000007
+using namespace std;
+// Longest Increasing Subsequence Problem
+// Given an array of integers, find the length of the longest increasing subsequence in the array.
+// Time complexity: O(2^n) and Space complexity: O(n)
+int recursion(int ind,int last,int n,vector<int>&arr)
+{
+    if(ind==n) return 0;
+    int not_take=recursion(ind+1,last,n,arr);
+    int take=0;
+    if(last==-1 || arr[ind]>arr[last])
+    {
+        take=1+recursion(ind+1,ind,n,arr);
+    }
+    return max(take,not_take);
+}
+// Time Complexity: O(n^2) and Space Complexity: O(n^2)
+int memorization(int ind,int last,int n,vector<int>&arr,vector<vector<int>>&dp)
+{
+    if(ind==n) return 0;
+    if(dp[ind][last+1]!=-1) return dp[ind][last+1];
+    int not_take=memorization(ind+1,last,n,arr,dp);
+    int take=0;
+    if(last==-1 || arr[ind]>arr[last])
+    {
+        take=1+memorization(ind+1,ind,n,arr,dp);
+    }
+    return dp[ind][last+1]=max(take,not_take);
+}
+int main()
+{
+    int n;
+    cin>>n;
+    vector<int>arr(n);
+    for(int i=0;i<n;i++) cin>>arr[i];
+    cout<<"Answer using the recursion: "<<recursion(0,-1,n,arr)<<endl;
+    vector<vector<int>>dp(n,vector<int>(n+1,-1));
+    cout<<"Answer using memorization: "<<memorization(0,-1,n,arr,dp)<<endl;
+}
+/*
+8
+10 9 2 5 30 70 101 188
+*/

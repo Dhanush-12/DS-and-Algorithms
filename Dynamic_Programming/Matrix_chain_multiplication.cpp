3f9dd6794497752cce2ed96f2cc2b2d43e43bb96
Dhanush-12 <dhanushdasarla@gmail.com>
2025-05-14 15:04:17 +0530

    Matrix chain multiplication recursion and memorization code added

---
+++ Dynamic_Programming/Matrix_chain_multiplication.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+#define inf 1000000007
+using namespace std;
+//Given a chain of matrices A1,..., An denoted by an array of size n+1, find out the minimum number of operations to multiply these n matrices.
+// Time complexity: O(n^3)
+int recursion(int i,int j,vector<int>&arr)
+{
+    if(i==j) return 0;
+    int mini=1000000007;
+    for(int k=i;k<j;k++)
+    {
+        int steps=arr[i-1]*arr[k]*arr[j] + recursion(i,k,arr) + recursion(k+1,j,arr);
+        if(steps<mini) mini=steps;
+    }
+    return mini;
+}
+// Time complexity: O(n^3) using memorization
+// Space complexity: O(n^2) + O(n) for dp array
+int memorization(int i,int j,vector<int>&arr,vector<vector<int>>&dp)
+{
+    if(i==j) return 0;
+    if(dp[i][j] != -1) return dp[i][j];
+    int mini=1000000007;
+    for(int k=i;k<j;k++)
+    {
+        int steps=arr[i-1]*arr[k]*arr[j] + memorization(i,k,arr,dp) + memorization(k+1,j,arr,dp);
+        if(steps<mini) mini=steps;
+    }
+    return dp[i][j]=mini;
+}
+int main()
+{
+    int n;
+    cin>>n;
+    vector<int>arr(n);
+    for(int i=0;i<n;i++) cin>>arr[i];
+    cout<<"Answer using recursion: "<<recursion(1,n-1,arr)<<endl;
+    vector<vector<int>>dp(n,vector<int>(n,-1));
+    cout<<"Answer using memorization: "<<memorization(1,n-1,arr,dp)<<endl;
+}
+/*
+5
+1 3 5 4 7
+*/

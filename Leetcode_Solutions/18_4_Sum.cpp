62c98fbdeee08b2d803590424a71971e0f7a17c1
Dhanush <dhanushdasarla@gmail.com>
2026-04-25 17:18:01 +0530

    File formatted

--- Leetcode_Solutions/18_4_Sum.cpp
+++ Leetcode_Solutions/18_4_Sum.cpp
@@ -1,42 +1,54 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-class Solution {
+class Solution
+{
 public:
-    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+    vector<vector<int>> fourSum(vector<int> &nums, int target)
+    {
 
         int n = nums.size();
         vector<vector<int>> ans;
 
         sort(nums.begin(), nums.end());
 
-        for(int i = 0; i < n - 3; i++) {
+        for (int i = 0; i < n - 3; i++)
+        {
 
-            if(i > 0 && nums[i] == nums[i - 1]) continue;
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
 
-            for(int j = i + 1; j < n - 2; j++) {
+            for (int j = i + 1; j < n - 2; j++)
+            {
 
-                if(j > i + 1 && nums[j] == nums[j - 1]) continue;
+                if (j > i + 1 && nums[j] == nums[j - 1])
+                    continue;
 
                 int left = j + 1, right = n - 1;
 
-                while(left < right) {
+                while (left < right)
+                {
 
                     long long sum = (long long)nums[i] + nums[j] + nums[left] + nums[right];
 
-                    if(sum == target) {
+                    if (sum == target)
+                    {
 
                         ans.push_back({nums[i], nums[j], nums[left], nums[right]});
 
                         left++;
                         right--;
 
-                        while(left < right && nums[left] == nums[left - 1]) left++;
-                        while(left < right && nums[right] == nums[right + 1]) right--;
+                        while (left < right && nums[left] == nums[left - 1])
+                            left++;
+                        while (left < right && nums[right] == nums[right + 1])
+                            right--;
                     }
-                    else if(sum < target) {
+                    else if (sum < target)
+                    {
                         left++;
                     }
-                    else {
+                    else
+                    {
                         right--;
                     }
                 }
@@ -49,19 +61,20 @@ public:
 int main()
 {
     int n;
-    cin>>n;
-    vector<int>arr(n);
-    for(int i=0;i<n;i++) cin>>arr[i];
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
     int k;
-    cin>>k;
+    cin >> k;
     Solution s;
     vector<vector<int>> ans = s.fourSum(arr, k);
-    for(int i=0;i<ans.size();i++)
+    for (int i = 0; i < ans.size(); i++)
     {
-        for(int j=0;j<ans[i].size();j++)
+        for (int j = 0; j < ans[i].size(); j++)
         {
-            cout<<ans[i][j]<<" ";
+            cout << ans[i][j] << " ";
         }
-        cout<<endl;
+        cout << endl;
     }
 }

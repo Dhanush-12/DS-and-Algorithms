8faa6f85dd81824ea2d8af023eec20ea7df8602c
Dhanush <dhanushdasarla@gmail.com>
2026-04-25 17:16:43 +0530

    Leetcode problem 4 sum code added

---
+++ Leetcode_Solutions/18_4_Sum.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+
+        int n = nums.size();
+        vector<vector<int>> ans;
+
+        sort(nums.begin(), nums.end());
+
+        for(int i = 0; i < n - 3; i++) {
+
+            if(i > 0 && nums[i] == nums[i - 1]) continue;
+
+            for(int j = i + 1; j < n - 2; j++) {
+
+                if(j > i + 1 && nums[j] == nums[j - 1]) continue;
+
+                int left = j + 1, right = n - 1;
+
+                while(left < right) {
+
+                    long long sum = (long long)nums[i] + nums[j] + nums[left] + nums[right];
+
+                    if(sum == target) {
+
+                        ans.push_back({nums[i], nums[j], nums[left], nums[right]});
+
+                        left++;
+                        right--;
+
+                        while(left < right && nums[left] == nums[left - 1]) left++;
+                        while(left < right && nums[right] == nums[right + 1]) right--;
+                    }
+                    else if(sum < target) {
+                        left++;
+                    }
+                    else {
+                        right--;
+                    }
+                }
+            }
+        }
+
+        return ans;
+    }
+};
+int main()
+{
+    int n;
+    cin>>n;
+    vector<int>arr(n);
+    for(int i=0;i<n;i++) cin>>arr[i];
+    int k;
+    cin>>k;
+    Solution s;
+    vector<vector<int>> ans = s.fourSum(arr, k);
+    for(int i=0;i<ans.size();i++)
+    {
+        for(int j=0;j<ans[i].size();j++)
+        {
+            cout<<ans[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
